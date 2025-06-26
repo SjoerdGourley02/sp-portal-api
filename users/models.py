533502fd232a5fa94feacd01aac1702853f4810a
Sjoerd Gourley <sjoerdgourley02@gmail.com>
2025-06-26 11:46:28 +0200

    adjust auth cookie lifetime

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import (
 
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, date_of_birth=None, password=None, **kwargs):
+    def create_user(self, email, password=None, **kwargs):
         if not email:
             raise ValueError("Users must have an email address")
         
@@ -16,7 +16,6 @@ class UserAccountManager(BaseUserManager):
 
         user = self.model(
             email=email,
-            date_of_birth=date_of_birth,
             **kwargs
         )
 
@@ -24,11 +23,10 @@ class UserAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, date_of_birth, password=None, **kwargs):
+    def create_superuser(self, email, password=None, **kwargs):
         user = self.create_user(
             email,
             password=password,
-            date_of_birth=date_of_birth,
             **kwargs,
         )
 
@@ -43,7 +41,6 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True, max_length=255)
-    date_of_birth = models.DateField()
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -53,7 +50,7 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'date_of_birth']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
